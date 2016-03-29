1d8a46647543cc34607cfd0e6a61e1b1636f8c5e
euforia <euforia@gmail.com>
2016-03-28 19:56:01 -0700

    Updated

--- ai/docker-bootstrap.sh
+++ ai/docker-bootstrap.sh
@@ -2,6 +2,8 @@
 #
 # Bootstrapping a linux box with docker and consul.
 #
+CONSUL_MASTER_URL="$1"
+
 INSTANCE_ID=""
 INSTANCE_PRIV_IP=""
 INSTANCE_REGION=""
@@ -17,18 +19,42 @@ CONSUL_ARGS="-node $(hostname) -ui-dir /ui -config-dir /data/config"
 # Only used by consul agents running in containers
 CONSUL_DOCKER_AGENT_ARGS="-v /var/run/docker.sock:/var/run/docker.sock"
 
-curl -s -O -L https://github.com/stedolan/jq/releases/download/jq-1.5/jq-linux64 && chmod +x jq-linux64 && mv ./jq-linux64 /usr/local/bin/jq
+#### Routines ####
 
-[ -d "${CONSUL_HOST_CFG_DIR}" ] || mkdir -p "${CONSUL_HOST_CFG_DIR}"
+set_consul_retry_join() {
+	if [ "${CONSUL_MASTER_URL}" != "" ]; then
+		for i in `curl -s "${CONSUL_MASTER_URL}" | jq -r .[].Address`; do 
+			CONSUL_ARGS="${CONSUL_ARGS} -retry-join ${i}";
+		done
+	fi
+}
+
+setup_consul_dirs() {
+	[ -d "${CONSUL_HOST_CFG_DIR}" ] || mkdir -p "${CONSUL_HOST_CFG_DIR}"
+}
+
+install_jq() {
+	JQ_URL="https://github.com/stedolan/jq/releases/download/jq-1.5/jq-linux64"
+	curl -s -O -L ${JQ_URL} && chmod +x jq-linux64 && mv ./jq-linux64 /usr/local/bin/jq || {
+		echo "Failed to install: jq"
+		return $?
+	}
+	return $?
+}
+
+#### Main ####
 
 if [ -e /etc/system-release ]; then
   if [ "$(cat /etc/system-release)" == "Amazon Linux AMI release 2016.03" ]; then
     INSTANCE_ID=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
     INSTANCE_PRIV_IP=$(curl -s http://169.254.169.254/latest/meta-data/local-ipv4)
-    INSTANCE_REGION=$(curl -s http://169.254.169.254/latest/dynamic/instance-identity/document|grep region|awk -F\" '{print $4}')
+    #INSTANCE_REGION=$(curl -s http://169.254.169.254/latest/dynamic/instance-identity/document|grep region|awk -F\" '{print $4}')
+    INSTANCE_REGION=$(curl -s http://169.254.169.254/latest/dynamic/instance-identity/document|jq -r .region)
 
     /bin/rpm -qa | grep docker || { /usr/bin/yum -y install docker && /sbin/service docker start; }
     
     CONSUL_ARGS="${CONSUL_ARGS} -dc ${INSTANCE_REGION} -advertise ${INSTANCE_PRIV_IP}"
   fi
 fi
+
+install_jq
